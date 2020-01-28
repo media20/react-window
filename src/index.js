c5009a481714c8b356edda26b58cf929e1a54227
Anatoliy Shishkin <anatoliy.shishkin@wikimart.ru>
2020-01-28 19:07:45 +0300

    react-window schedule part

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 export { default as VariableSizeGrid } from './VariableSizeGrid';
 export { default as VariableSizeList } from './VariableSizeList';
 export { default as FixedSizeGrid } from './FixedSizeGrid';
+export { default as FixedSizeGridSchedule } from './FixedSizeGridSchedule';
 export { default as FixedSizeList } from './FixedSizeList';
 
 export { default as areEqual } from './areEqual';
